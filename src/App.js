63bccd4f65dfb0dc59eb104d760aa9378404f74e
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-03-17 16:37:38 -0300

    Create Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+
 function App() {
   return (
     <div>
-      App React
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
