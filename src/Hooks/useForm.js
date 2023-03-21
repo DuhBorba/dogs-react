0ba77eb6c73955f61d993c678f7adcf4b86153fd
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-03-21 17:12:58 -0300

    Create User

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const types = {
   email: {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
-  }
-}
+  },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.',
+  },
+};
 
 const useForm = (type) => {
   const [value, setValue] = React.useState('');
-  const [error, setError] = React.useState(null)
+  const [error, setError] = React.useState(null);
 
-  function validate(value){
-    if(type === false) return true;
-    if(value.length === 0){
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
       setError('Preencha um valor.');
       return false;
-    } else if(types[type] && !types[type].regex.test(value)){
+    } else if (types[type] && !types[type].regex.test(value)) {
       setError(types[type].message);
       return false;
     } else {
@@ -25,10 +30,8 @@ const useForm = (type) => {
     }
   }
 
-  function onChange({target}){
-    if(error){
-      validate(target.value);
-    }
+  function onChange({ target }) {
+    if (error) validate(target.value);
     setValue(target.value);
   }
 
@@ -38,8 +41,8 @@ const useForm = (type) => {
     onChange,
     error,
     validate: () => validate(value),
-    onBlur: () => validate(value)
-  }
-}
+    onBlur: () => validate(value),
+  };
+};
 
 export default useForm;
