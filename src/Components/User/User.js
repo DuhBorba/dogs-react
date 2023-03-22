9a693e7636e28567db609a553a509132cbc6bb1c
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-03-22 15:26:23 -0300

    User Header

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Feed from '../Feed/Feed';
+import { Route, Routes } from 'react-router-dom';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
+import UserHeader from './UserHeader';
 
 const User = () => {
   return (
-    <div>User</div>
+    <section className='container'>
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+      </Routes>
+    </section>
   )
 }
 
