30547fd2cdd27dda32d5662b60249fb9142239e2
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-03-24 15:08:11 -0300

    Delete Foto

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import PhotoComments from './PhotoComments';
+import { UserContext } from '../../UserContext';
 import styles from './PhotoContent.module.css'
+import PhotoDelete from './PhotoDelete';
 
 const PhotoContent = ({data}) => {
+  const user = React.useContext(UserContext);
   const {photo, comments} = data;
   return (
     <div className={styles.photo}>
@@ -13,7 +16,11 @@ const PhotoContent = ({data}) => {
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`} >@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? ( 
+              <PhotoDelete id={photo.id} />
+              ) : (
+              <Link to={`/perfil/${photo.author}`} >@{photo.author}</Link>
+            )}
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
           <h1 className='title'>
@@ -21,7 +28,7 @@ const PhotoContent = ({data}) => {
           </h1>
           <ul className={styles.attributes}>
             <li>{photo.peso} kg</li>
-            {photo.idade === 1 ? (
+            {photo.idade == 1 ? (
               <li>{photo.idade} ano</li>
               ) : (
               <li>{photo.idade} anos</li>
